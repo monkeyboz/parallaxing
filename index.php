0f5b806b72b883121a7e3f4f5d6cd9c2530e3a32
Monkeyboz <taurean.wooley@gmail.com>
2014-10-24 09:19:02 -0700

    commit all changes

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script src="js/script.js"></script>
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+	</head>
+	<body>
+		<header>
+			<navigation id=""></navigation>
+		</header>
+		<main>
+			<article>
+				<div id="info"></div>
+				<div id="foreground">
+					<img src="http://doginstructions.com/wp-content/uploads/2014/07/birds-flying-in-the-sky-silhouettebird-silhouette-wallpaper---wallpaper-panda-hphkjhzu.png" style="width: 1200px; height: 900px;"/>
+				</div>
+				<div id="mainground">
+					<div>
+						<div class="content" style="width: 640px; height: 400px;"><img src="images/logo.png"/></div>
+					</div>
+				</div>
+				<div id="background">
+					<img src="http://pngimg.com/upload/cloud_PNG4.png" style="width: 1200px; height: 900px;"/>
+				</div>
+			</article>
+		</main>
+	</body>
+</html>
